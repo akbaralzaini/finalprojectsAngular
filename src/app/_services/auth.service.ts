a3d943c4e7728e1c261d5227dfff1aa2faa1127b
akbar <akbaralzaini@gmail.com>
2021-06-06 11:37:57 +0700

    initial

---
+++ src/app/_services/auth.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const AUTH_API = 'http://localhost:8080/api/auth/';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  constructor(private http: HttpClient) { }
+
+  login(username: string, password: string): Observable<any> {
+    return this.http.post(AUTH_API + 'auth', {
+      username,
+      password
+    }, httpOptions);
+  }
+
+  register(username: string, firstName: string, lastName:string,mobileNumber:string, role:any, password: string): Observable<any> {
+    return this.http.post(AUTH_API + 'v1/user/signup', {
+      username,
+      password,
+      firstName,
+      lastName,
+      mobileNumber,
+      role
+    }, httpOptions);
+  }
+
+  registerAgency(user_id : number,name: string, details:string,code:string  ): Observable<any>{
+    return this.http.post('http://localhost:8080/api/v1/agency/',{
+      user_id,
+      name,
+      details,
+      code
+    }, httpOptions); 
+  }
+}
---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { TokenStorageService } from '../_services/token-storage.service';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+  form: any = {
+    username: null,
+    email: null,
+    password: null
+  };
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = '';
+
+
+  constructor(private authService: AuthService, private tokenStorage: TokenStorageService) { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    const { username, password,firstName, lastName, agency,details, mobileNumber } = this.form;
+
+    this.authService.register(username, firstName,lastName,mobileNumber,['admin'] , password).subscribe(
+      data => {
+        console.log(data);
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+        console.log(data.id);
+        this.registerAgency(data.id,agency, details);
+      },
+      err => {
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
+      }
+    );
+  }
+
+  registerAgency(user_id:number, name:string, details: string):void{
+    this.authService.registerAgency(user_id,name,details,"A-"+name).subscribe(
+      data =>{
+        console.log(data);
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+      },
+      err=>{
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
+      } 
+    );
+  }
+
+}
